8650217b83ab626016da4c6aadde7b9dc34217eb
Kartik Bhalla <kartikbhalla12@gmail.com>
2021-07-06 16:25:25 +0530

    src: cleanup & add navbar
    Signed-off-by: Kartik Bhalla <kartikbhalla12@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import styled from 'styled-components';
+import Navbar from './components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export interface AppProps {}
+
+const Wrapper = styled.div`
+	margin: 0 5rem;
+	font-family: 'Poppins', sans-serif;
+	font-weight: 600;
+`;
+
+const App: React.FC<AppProps> = () => {
+	return (
+		<Wrapper>
+			<Navbar />
+		</Wrapper>
+	);
+};
 
 export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,79 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+export interface NavbarProps {}
+
+const Nav = styled.section`
+	display: flex;
+	justify-content: space-between;
+	padding: 1rem 0;
+	font-size: 0.9rem;
+`;
+
+const LoginParent = styled.div`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	font-size: 0.9rem;
+`;
+
+const SignInButton = styled.div`
+	color: #939398;
+	padding: 0.25rem 1rem;
+	border-radius: 1rem;
+	margin-right: 1rem;
+	cursor: pointer;
+`;
+
+const SignUpButton = styled.div`
+	background-color: #2acfcf;
+	color: white;
+	padding: 0.4rem 1rem;
+	border-radius: 10rem;
+	cursor: pointer;
+`;
+
+const LeftParent = styled.div`
+	display: flex;
+	flex-direction: row;
+`;
+
+const Logo = styled.div`
+	font-size: 1.6rem;
+	font-weight: 800;
+	margin-right: 1.5rem;
+`;
+
+const CategoryParent = styled.div`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+`;
+
+const Category = styled.div`
+	margin: 0 1rem;
+	color: #939398;
+	font-size: 0.9rem;
+	cursor: pointer;
+`;
+
+const Navbar: React.FC<NavbarProps> = () => {
+	return (
+		<Nav>
+			<LeftParent>
+				<Logo>Shortly</Logo>
+				<CategoryParent>
+					<Category>Features</Category>
+					<Category>Pricing</Category>
+					<Category>Resources</Category>
+				</CategoryParent>
+			</LeftParent>
+			<LoginParent>
+				<SignInButton>Login</SignInButton>
+				<SignUpButton>Sign Up</SignUpButton>
+			</LoginParent>
+		</Nav>
+	);
+};
+
+export default Navbar;
